0294200617e802c69651bce42648aae80b61d5d4
hyunjoebrother <66728383+hyunjoebrother@users.noreply.github.com>
2020-11-25 22:58:09 +0900

    실습2 HelloServlet, NameServlet

---
+++ HelloServlet.java
@@ -0,0 +1,40 @@
+package examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset = UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Hello World!</h1>");
+		
+		//이 부분에 Ctrl+Shift+O 누르면 자동으로 import된다
+		
+		//Servlet은 JAVA 코드 안에 HTML 코드를 작성할 수 있는 JAVA program이다.
+	}
+
+}
